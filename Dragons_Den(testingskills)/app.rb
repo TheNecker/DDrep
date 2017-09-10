e34606613f2ae004199c94458758c722181b2750
Christopher Phelps <chrisp132012@gmail.com>
2017-09-09 22:36:16 -0400

    Add files via upload

---
+++ Dragons_Den(testingskills)/app.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+
+get '/' do 
+	erb :index
+end 
+
+get '/events' do 
+	erb :events
+end 
+
+get '/contactUs' do 
+	erb :contactUs
+end 
+
+get '/aboutUs' do 
+	erb :aboutUs
+end 
